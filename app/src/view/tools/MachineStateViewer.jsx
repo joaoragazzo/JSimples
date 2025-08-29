81e45466437d3d954426ab91fc8ce38732a1152e
joaoragazzo <joao_ragazzo@hotmail.com>
2025-08-29 09:10:17 -0300

    fix: fix horizontal dimension bigger on windows

--- app/src/view/tools/MachineStateViewer.jsx
+++ app/src/view/tools/MachineStateViewer.jsx
@@ -97,6 +97,8 @@ const ArrowCell = styled.div`
 
 const StyledTableInstructions = styled(Table)`
   overflow-y: auto;
+  max-width: 100%;
+  width: 100%;
 `;
 
 const ControlBar = styled.div`
@@ -111,6 +113,7 @@ const TableWrapper = styled.div`
   display:flex;
   flex-direction: column;
   gap: 30px;
+  overflow: hidden;
 `
 const MarginRightArrow = styled(FaArrowRight)`
   margin-right: 30px;
